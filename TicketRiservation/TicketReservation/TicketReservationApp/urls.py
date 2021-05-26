d3847103b42a942874973189032e7d85fd5ce353
abhijitmorye <abhijitmorye>
2021-05-26 23:58:04 +0530

    TicketReservation - Need To Think of Entire Website design Plan - 26052021

--- TicketRiservation/TicketReservation/TicketReservationApp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from TicketReservationApp import views
-
-app_name  = 'TicketReservationApp'
-urlpatterns = [
-    path('', views.index, name='index'),
-
-]
