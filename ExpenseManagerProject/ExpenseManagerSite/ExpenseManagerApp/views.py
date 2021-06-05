f96ca5c12b59aa4861a22f46a1d3295237f98d90
abhijitmorye <abhijitmorye>
2021-06-05 11:57:48 +0530

    Django Expense Manager - Completed - 05062021

--- ExpenseManagerProject/ExpenseManagerSite/ExpenseManagerApp/views.py
+++ ExpenseManagerProject/ExpenseManagerSite/ExpenseManagerApp/views.py
@@ -6,6 +6,7 @@ from .models import Expenses
 
 
 def index(request):
+    flag = False
     if request.method == 'POST':
         expenseName = request.POST.get('expenseName')
         expenseAmount = request.POST.get('expenseAmount')
@@ -17,4 +18,39 @@ def index(request):
         expense.save()
         return redirect('/')
 
-    return render(request, 'ExpenseManagerApp/index.html')
+    exepenes = Expenses.objects.all()
+    total = 0
+    totalFood = 0
+    totalEnt = 0
+    totalTravel = 0
+    totalBusi = 0
+    totalOther = 0
+    if len(exepenes) == 0:
+        pass
+    else:
+        flag = True
+        for expense in exepenes:
+            if expense.expenseCategory == 'Travel':
+                totalTravel += expense.expenseAmount
+            elif expense.expenseCategory == 'Food':
+                totalFood += expense.expenseAmount
+            elif expense.expenseCategory == 'Entertainment':
+                totalEnt += expense.expenseAmount
+            elif expense.expenseCategory == 'Business':
+                totalBusi += expense.expenseAmount
+            elif expense.expenseCategory == 'Other':
+                totalOther += expense.expenseAmount
+            total += expense.expenseAmount
+
+    context = {
+        "flag": flag,
+        "expenses": exepenes,
+        "total": total,
+        'totalTravel': totalTravel,
+        'totalFood': totalFood,
+        'totalEnt': totalEnt,
+        'totalBusi': totalBusi,
+        'totalOther': totalOther
+    }
+
+    return render(request, 'ExpenseManagerApp/index.html', context=context)
